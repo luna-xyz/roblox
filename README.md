14395e2cc0a6d6401f1fed69f6d934a0e2b6b26e
luna-xyz <l08091710@gmail.com>
2025-03-14 15:25:42 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# Luna.xyz
+This is the official repository for Luna.xyz
+- Other repostories are fake this is the official by "[luna-xyz](https://github.com/luna-xyz)"
+
+## Loadstring
+The current loadstring is:
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/luna-xyz/roblox/refs/heads/main/main.lua"))()
+```
+## Compatibility
+Luna is compatible with the following:
+
+**PC Executors:**
+* Wave
+* Swift
+* Synapse z
+
+**Mobile Executors:**
+* Krnl
+* Codex
+
+## More Info
+Visit our Official Discord server [here](https://discord.gg/).
---
+++ main.lua
@@ -0,0 +1,49 @@
+if getgenv().hub_enviorment then
+	return warn("luna.xyz is already loaded!")
+end
+
+local supported_games = {
+	["rakoof"] = 15367709476,
+};
+
+getgenv().hub_enviorment = {
+	
+	Signals = {},
+	hub_version = game:HttpGet("https://raw.githubusercontent.com/luna-xyz/roblox/refs/heads/main/version.txt");
+};
+
+function hub_enviorment:AddSignal(s, __f)
+	table.insert(hub_enviorment.Signals, s:Connect(__f));
+end;
+
+function hub_enviorment:DisconnectAll()
+
+	for Idx = #hub_enviorment.Signals, 1, -1 do
+
+		local Connection = table.remove(hub_enviorment.Signals, Idx);
+		Connection:Disconnect();
+	end;
+end;
+
+function hub_enviorment:Notify(text)
+
+	game:GetService("StarterGui"):SetCore("SendNotification", {
+
+		Title = 'luna.xyz ' .. tostring(hub_enviorment.hub_version),
+		Text = tostring(text),
+
+		Duration = 3,
+	})
+end;
+
+for i, v in supported_games do
+	
+	if tostring(game.PlaceId) == tostring(v) then
+		return loadstring(game:HttpGet('https://raw.githubusercontent.com/luna-xyz/roblox/refs/heads/main/games/' .. tostring(i) .. '.lua'))();
+	end;
+end;
+
+getgenv().hub_enviorment = nil;
+
+local LP = game:GetService("Players").LocalPlayer;
+pcall(function() LP:Kick('This game is not supported!\n\nVersion: ' .. tostring(hub_enviorment.hub_version)) end);
