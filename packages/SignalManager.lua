fb8c60dba4a61a33fd48755dbf581981861e291b
luna-xyz <l08091710@gmail.com>
2025-08-12 14:18:56 -0600

    Update SignalManager.lua

--- packages/SignalManager.lua
+++ packages/SignalManager.lua
@@ -3,43 +3,83 @@
 local SignalManager = {};
 SignalManager.__index = SignalManager;
 
-function SignalManager.__init()
-	
-	local self = setmetatable({}, SignalManager);
-	self._connections = {};
-	
-	return self;
-end;
-
 function SignalManager:AddSignal(signal, callback)
-	
+
 	local connection = signal:Connect(callback);
-	table.insert(self._connections, connection);
-	
+	table.insert(self._signals, connection);
+
 	return connection;
 end;
 
-function SignalManager:Remove(connection)
-	
-	for i, v in ipairs(self._connections) do
-		
+function SignalManager:RemoveSignal(connection)
+
+	for i, v in ipairs(self._signals) do
+
 		if v ~= connection then continue end;
-		
+
 		v:Disconnect();
-		table.remove(self._connections, i);
-		
+		table.remove(self._signals, i);
+
 		break;
 	end;
 end;
 
+function SignalManager:AddThread(callback)
+
+	local thread = task.spawn(callback);
+	table.insert(self._threads, thread);
+
+	return thread;
+end;
+
+function SignalManager:RemoveThread(thread)
+
+	for i, v in ipairs(self._threads) do
+
+		if v ~= thread then continue end;
+
+		pcall(task.cancel, thread);
+		table.remove(self._threads, i);
+
+		break;
+	end;
+end;
+
+function SignalManager:RemoveAllSignals()
+
+	for _, v in ipairs(self._signals) do
+		v:Disconnect();
+	end;
+
+	self._signals = {};
+end;
+
+function SignalManager:RemoveAllThreads()
+
+	for _, v in ipairs(self._threads) do
+		pcall(task.cancel, v);
+	end;
+	
+	self._threads = {};
+end;
 
 function SignalManager:RemoveAll()
-	
-	for _, v in ipairs(self._connections) do
+
+	for _, v in ipairs(self._signals) do
 		v:Disconnect();
 	end;
 	
-	self._connections = {};
-end
+	for _, v in ipairs(self._threads) do
+		pcall(task.cancel, v);
+	end;
 
-return SignalManager.__init();
+	self._signals = {}; self._threads = {};
+end;
+
+return function()
+	
+	local self = setmetatable({}, SignalManager);
+	self._signals = {}; self._threads = {};
+
+	return self;
+end;
